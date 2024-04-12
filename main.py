b5b3c62a7de4006f11d8eadcc4007f7463ae9f03
Pavelrh <shridharapavel@gmail.com>
2024-04-12 13:50:41 -0400

    created a function to calculuate loss

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('score.csv')
 
-plt.scatter(data.X, data.Y)
+# loss function is just the error function
+def lossFunction(m, b, points):
+    totalError = 0
+    for i in range(len(points)):
+        x = points.iloc[i].X
+        y = points.iloc[i].y
+        # ** 2 --> square
+        totalError += y - ((m * x) + b) ** 2
+        totalError = totalError / len(points)
 
-plt.show()
+
